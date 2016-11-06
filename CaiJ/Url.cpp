0a24fcbd4d03d1e6f5e3bf119757227ec4861050
williamtse <xwengf@126.com>
2016-11-07 00:09:57 +0800

    cc

--- CaiJ/Url.cpp
+++ CaiJ/Url.cpp
@@ -24,16 +24,29 @@ Url::GenerateUrl(){
 				+cuid+L"&rtype="+rtype+L"&langx=zh-cn&mtype=3&page_no="+pageNum;
 			break;
 		case L"reloadgame":
+			url = SITE+L"/app/member/browse_FS/reloadgame_R.php?FStype="+stype+L"&rtype="+rtype+L"&uid="
+						+uid+L"langx=zh-cn";
 			break;
 		case L"result":
+			SYSTEMTIME st; 　　
+			CString strDate,strTime; 　　
+			GetLocalTime(&st); 　　
+			strDate.Format("%4d-%2d-%2d",st.wYear,st.wMonth,st.wDay);
+			CString file;
 			switch(stype){
 				case 'FT':
 				case 'BK':
 				case 'BS':
 				case 'OP':
-					url = SITE+L"/app/member/result/result.php?game_type="+stype+L"&list_date="
+					file=L"result.php";
+				break;
+				default:
+					file=L"result_"+stype+L".php";
 					break;
 			}
+			url = SITE+L"/app/member/result/"+file+L"?game_type="+stype+L"&list_date="+strDate+L"&uid="
+						+uid+L"langx=zh-cn";
 			break;
 	}
+	return url;
 }
